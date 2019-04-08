71d423012f046c61980bb3b009339b1d7a1e9b97
ivk18 <ivk7bc@gmail.com>
2019-04-08 18:50:43 +0300

    Решена задача № 2 из 2-го урока (уровень easy)

---
+++ lesson-02-easy-02.py
@@ -0,0 +1,15 @@
+# урок 02, задание 2 (уровень easy)
+
+__author__ = 'Караваев Илья Викторович'
+
+list1 = ['пароль', 'файл', 2348, 'code', 'type', 'qwerty', 'message']   # 6
+list2 = ['type', 'qwerty', 'стиль', 234, 'mes']                         # 4
+list3 = []
+
+print('Список 1:\n' + str(list1))
+print('\nСписок 2:\n' + str(list2))
+
+for l1 in list1:
+    if l1 not in list2: list3.append(l1)
+
+print('\nСписок 1 без элементов списка 2:\n' + str(list3) + '\n')
