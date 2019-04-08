e9f68e21e2d78896354fe8d36c41d2094c77567c
ivk18 <ivk7bc@gmail.com>
2019-04-08 19:28:51 +0300

    Решение без использования range()

--- lesson-02-easy-03.py
+++ lesson-02-easy-03.py
@@ -5,11 +5,11 @@ __author__ = 'Караваев Илья Викторович'
 list1 = [23, 2, 12, 95, 76, 69, 64, 37, 49, 56]
 list2 = []
 
-for i in range(len(list1)):
-    if list1[i] % 2 == 0:
-        list2.append(list1[i] / 4)
+for l in list1:
+    if l % 2 == 0:
+        list2.append(l / 4)
     else:
-        list2.append(list1[i] * 2)
+        list2.append(l * 2)
         
 print('Исходный список:\n' + str(list1))
 print('\nНовый список:\n' + str(list2) + '\n')
