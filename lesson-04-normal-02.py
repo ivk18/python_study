3ea1c1f903b2fe53cfffbc2668568ac79c5e33a5
ivk18 <ivk7bc@gmail.com>
2019-04-19 14:35:00 +0300

    Решена задача № 2 из 4-го урока (уровень normal). Оба варианта решения перемещены в функции. for вместо while в решении без "re".

--- lesson-04-normal-02.py
+++ lesson-04-normal-02.py
@@ -28,11 +28,11 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
 ################################################
 print('Решение с использованием функции "re":' + str('\n'))
 
-def collectRe(s, before=2, after=2):
+def collectRe(S, before=2, after=2):
     import re
     result = []
     pattern = '[a-z]{' + str(before) + '}[A-Z]+[A-Z]{' + str(after) + '}'
-    for item in re.findall(pattern, s):
+    for item in re.findall(pattern, S):
         result.append(item[before:-after])
     return result
 
@@ -45,13 +45,13 @@ print(collectRe(line_2,1,5))
 ################################################
 print(str('\n') + 'Решение без использования функции "re":' + str('\n'))
 
-def collect(s, before=2, after=2):
+def collect(S, before=2, after=2):
     found = []  # список для итогового результата
     bigs = '' # строка для хранения найденной последовательности прописных букв
     i_small = 0     # счетчик строчных букв
     i_big = 0       # счетчик прописных букв
-    while s:
-        if s[0] != s[0].title(): # если первая буква строки - строчная
+    for s in S:
+        if s != s.title(): # если первая буква строки - строчная
             # если последовательность прописных удовлетворяет условию after
             if len(bigs) >= after + 1:
                 # добавляем найденную последовательность прописных к списку
@@ -61,15 +61,15 @@ def collect(s, before=2, after=2):
             i_big = 0
             # и строку с найденными прописными буквами
             bigs = ''  
-            s = s[1:]   # переходим к следующему символу
-        elif s[0] == s[0].title(): # если первая буква строки - прописная
+            S = S[1:]   # переходим к следующему символу
+        elif s == s.title(): # если первая буква строки - прописная
             # если уже найдена хотя бы одна прописная буква или 
             # счетчик строчных удовлетворяет условию before
             if i_small >= before or i_big > 0:
-                bigs += s[0]    # добавляем найденную прописную букву
+                bigs += s    # добавляем найденную прописную букву
                 i_big += 1      # и увеличиваем счетчик прописных
             i_small = 0 # обнуляем счетчик строчных
-            s = s[1:]   # переходим к следующему символу
+            S = S[1:]   # переходим к следующему символу
     return found
 
 print(collect(line_2))
