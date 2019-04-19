6bf86939511e8ba4c45cb049ef77425450fa2b22
ivk18 <ivk7bc@gmail.com>
2019-04-19 13:01:01 +0300

    Решена задача № 2 из 4-го урока (уровень normal). Решение без помощи функции "re".

--- lesson-04-normal-02.py
+++ lesson-04-normal-02.py
@@ -39,6 +39,27 @@ for item in re.findall(pattern, line_2):
 print(str(result) + str('\n'))
 
 
-# print('Решение без использования функции "re":' + str('\n'))
+print('Решение без использования функции "re":' + str('\n'))
 
-# в процессе...
+def collect(s, before=2, after=2):
+    found = []
+    bigs = ''
+    i_small = 0
+    i_big = 0
+    while s:
+        if s[0] != s[0].title(): # строчная
+            if len(bigs) >= after + 1:
+                found.append(bigs[:-after])
+            bigs = ''
+            i_small += 1
+            i_big = 0
+            s = s[1:]
+        elif s[0] == s[0].title(): # прописная
+            if i_small >= before or i_big > 0:
+                bigs += s[0]
+                i_big += 1
+            i_small = 0
+            s = s[1:]
+    return found
+
+print(collect(line_2))
