ac622d322d7d19050dc9fd1328544a16b474bad5
ivk18 <ivk7bc@gmail.com>
2019-04-27 00:11:40 +0300

    Решена задача № 3 из 4-го урока (уровень hard).

--- lesson-04-hard-03.py
+++ lesson-04-hard-03.py
@@ -24,19 +24,53 @@ def coordGen():
             quantity -= 1       
     return coord
 
-# отобразить положение ферзей на "доске"
-def showBoard():
-    coord = coordGen()
-    #print(str(coord) + str('\n'*2))    # отобразить координаты для проверки
+# отображаем положение ферзей на "доске"
+def showBoard(items):
+    # print(str(items) + str('\n'*2))    # отобразить координаты для проверки
     board = []
     [board.append([]) for i in range(8)]
     for i in range(8):
         for j in range(8):
-            if (i, j) in coord:
+            if (i, j) in items:
                 board[i].append("X")
             else:
                 board[i].append("O")
             print(board[i][j], end="  ")
         print('\n')
 
-showBoard()
+# проверка угрозы ферзей друг другу
+def checkAttack():
+    global flag
+    coord = coordGen()
+    showBoard(coord)
+    start_item = coord[0]
+    coord = coord[1:]
+    result = []
+    while coord:
+        for item in coord:
+            # если ферзи на одной горизонтали, вертикали или диагонали,
+            # то они угрожают друг другу
+            if (start_item[0] == item[0] or start_item[1] == item[1] or
+                abs(start_item[0] - item[0]) == abs(start_item[1] - item[1])):              
+                result.append(start_item)
+                result.append(item)
+        start_item = coord[0]
+        coord = coord[1:]
+    result = set(result)
+    result = list(result)
+    if not result: flag = False
+    return result
+
+flag = True   
+quantity = 0         
+import os
+            
+while flag:
+    os.system('cls')
+    print('Первоначальная расстановка ферзей: ' + str('\n'))            
+    res = checkAttack()
+    print(str('\n') + 'Ферзи под "боем": ' + str('\n') + str(res) + str('\n'*2))
+    print('Расстановка ферзей под "боем": ' + str('\n')) 
+    showBoard(res)
+    quantity += 1
+    print((str('\n'*2) + 'Проверено расстановок: ' + str(quantity)))
